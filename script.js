d596e131cb3f070659983e14401b5fd213c14643
Emerson <emerson.day@gmail.com>
2022-08-29 14:27:39 -0400

    displaying table

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let myLibrary = []
+
 
 function Book(title, author, pages, read){
  this.title = title
@@ -12,8 +12,33 @@ function Book(title, author, pages, read){
 }
 
 const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false)
-
+const fireAndIce = new Book('A Song of Fire and Ice', 'George R.R. Martin', 893, false)
 function addBookToLibrary(){
  newBook = new Book(title, authors, pages, read)
  myLibrary.push(newBook)
 }
+
+let myLibrary = [theHobbit, fireAndIce]
+let table = document.getElementById('di-library');
+
+const displayData = (array) => {
+ array.forEach(item => {
+ let tr = document.createElement('tr');
+
+ Object.entries(item).forEach(value => {
+  const [key, val] = value
+  
+  if(typeof val !== 'function'){
+   let td = document.createElement('td');
+   td.innerText = val
+   tr.appendChild(td);
+  }
+  
+ });
+ table.appendChild(tr);
+})
+}
+
+displayData(myLibrary);
+
+
