d67e234eab8bada0a4ce3dbdf70fa518558af26d
Emerson <emerson.day@gmail.com>
2022-08-29 17:01:36 -0400

    updated modal

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 function Book(title, author, pages, read){
  this.title = title
  this.author = author 
@@ -13,6 +11,7 @@ function Book(title, author, pages, read){
 
 const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false)
 const fireAndIce = new Book('A Song of Fire and Ice', 'George R.R. Martin', 893, false)
+
 function addBookToLibrary(){
  newBook = new Book(title, authors, pages, read)
  myLibrary.push(newBook)
@@ -21,7 +20,7 @@ function addBookToLibrary(){
 let myLibrary = [theHobbit, fireAndIce]
 let table = document.getElementById('di-library');
 
-const displayData = (array) => {
+function displayData(array){
  array.forEach(item => {
  let tr = document.createElement('tr');
 
@@ -41,4 +40,49 @@ const displayData = (array) => {
 
 displayData(myLibrary);
 
+var outputParagraph = document.querySelector('#quote')
 
+var buttons = document.querySelectorAll('button')
+var modal = document.querySelector('.modal')
+var backdrop = document.querySelector('.backdrop')
+
+var textEdit = document.querySelector('.modal textarea')
+var quote = 'a modal component on a webpage --- can you do it?'
+var editedQuote = ''
+
+function updateParagraph(){
+ outputParagraph.textContent = quote;
+}
+
+function closeModal(){
+ modal.style.display = 'none'
+ backdrop.style.display = 'none'
+}
+updateParagraph();
+
+
+
+buttons[2].addEventListener('click', function(){
+ modal.style.display = 'block';
+ backdrop.style.display = 'block';
+ editedQuote = quote;
+ textEdit.value = quote;
+});
+
+// backdrop.addEventListener('click', closeModal)
+buttons[0].addEventListener('click', closeModal);
+
+buttons[1].addEventListener('click', function(){
+ closeModal();
+ if (editedQuote.trim().length > 0){
+  quote = editedQuote;
+  updateParagraph();
+ }
+
+ 
+});
+
+
+textEdit.addEventListener('input', function(){
+ editedQuote = textEdit.value
+})
